ed243aeeb9a79dc5c2d0ec98d01504847e302006
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-03-04 11:21:09 +0530

    submit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('dotenv').config() //envirnment variables
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: "true",
 }))
 app.use(cookieParser())
