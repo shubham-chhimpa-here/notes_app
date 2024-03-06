b785dd2e0677ce38dad3313705e3495fdca1b3f1
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-03-06 10:19:58 +0530

    submit: update and deleted route

--- router/blog.router.js
+++ router/blog.router.js
@@ -3,20 +3,39 @@ const { BlogModel } = require('../models/blog.model')
 const blogRouter = express.Router()
 
 
-blogRouter.get('/', async (req,res) => {
-    const {userId} = req.body;
-    const data = await BlogModel.find({ userId : userId});
-    
-    res.send({msg: 'blog data', login: true , data})
+blogRouter.get('/', async (req, res) => {
+    const { userId } = req.body;
+    const data = await BlogModel.find({ userId: userId });
+
+    res.send({ msg: 'blog data', login: true, data })
+})
+
+blogRouter.get('/:blogId', async (req, res) => {
+    const { blogId } = req.params;
+    const data = await BlogModel.findOne({ _id: blogId })
+    res.send({ msg: 'blog', data })
 })
 
 blogRouter.post('/create', async (req, res) => {
     const { title, content, userId } = req.body;
-    
-    const blog = BlogModel({title, content, userId});
+    const blog = BlogModel({ title, content, userId });
     const response = await blog.save()
-    res.send({msg: 'new blog has been added', response})
+    res.send({ msg: 'new blog has been added', response })
 
+})
+
+blogRouter.patch('/update/:blogId', async (req, res) => {
+    const { title, content, userId } = req.body;
+    const { blogId } = req.params;
+
+    await BlogModel.findByIdAndUpdate(blogId, { title, content, userId })
+    res.send({ msg: 'blog has been updated' })
+})
+
+blogRouter.delete('/delete/:blogId', async (req, res) => {
+    const { blogId } = req.params;
+    await BlogModel.findByIdAndDelete(blogId);
+    res.send({ msg: 'blog has been deleted' });
 })
 module.exports = {
     blogRouter
